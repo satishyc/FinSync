fa3e2d6dadf5c96d6960277966626f010d81f64e
Satish Y C <sryc123@gmail.com>
2024-06-07 22:24:16 +0530

    FinSync Initial Commit

---
+++ src/main/java/com/example/FinSync/FinSyncApplication.java
@@ -0,0 +1,13 @@
+package com.example.FinSync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinSyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinSyncApplication.class, args);
+	}
+
+}
